2194f754845afdccfac3a1640c7f69b7e9f56a0a
ericmbouwe <ericmbouwe@yahoo.fr>
2020-09-22 10:54:56 +0100

    added buttons components in the Button panel

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default function Button(props) {
-  return <div></div>;
+  return <div>{props.name}</div>;
 }
 
 Button.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired,
 };
