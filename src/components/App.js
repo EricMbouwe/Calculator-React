d092abcf3be1b6ebbd7891726c5523cbc97bd15e
ericmbouwe <ericmbouwe@yahoo.fr>
2020-09-24 18:23:53 +0100

    changed the App component from sfc to statefull component

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import Display from './Display';
-import ButtonPanel from './ButtonPanel';
+import React from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
 // eslint-disable-next-line
-import calculate from '../logic/calculate';
-import '../App.css';
+import calculate from "../logic/calculate";
+import "../App.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
+
+export default App;
